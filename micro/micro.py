f31e1e675d7c4010886d5ec425b8cbdb13c85d9a
EKnight-Blue <loicchechevalier@gmail.com>
2023-08-28 17:12:57 +0200

    scans

--- micro/micro.py
+++ micro/micro.py
@@ -2,3 +2,14 @@ import serial
 from serial.tools.list_ports import comports
 
 
+class MicroManager:
+    def __init__(self):
+        print(*self.scan(), sep='\n')
+
+    @staticmethod
+    def scan():
+        return tuple(serial.Serial(usb.device, 115200) for usb in comports())
+
+
+if __name__ == '__main__':
+    MicroManager()
