54bb5fb14f3df41a04c24b05fb3c15690c87afed
EKnight-Blue <loicchechevalier@gmail.com>
2023-09-06 12:51:37 +0200

    dumb loic

--- ps_controll.py
+++ ps_controll.py
@@ -5,7 +5,8 @@ if __name__ == '__main__':
     c = Controller()
     c.start()
     try:
-        for ev in c.get_events():
-            print(ev)
+        while True:
+            for ev in c.get_events():
+                print(ev)
     except KeyboardInterrupt:
         c.terminate()
