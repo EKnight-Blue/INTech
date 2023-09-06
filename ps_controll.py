f153a87ab6bd4268267e904758e2599af7de0f14
EKnight-Blue <loicchechevalier@gmail.com>
2023-09-06 13:56:44 +0200

    dumb loic

--- ps_controll.py
+++ ps_controll.py
@@ -46,11 +46,4 @@ class RobotController(Controller, MicroManager):
 
 
 if __name__ == '__main__':
-    c = Controller()
-    c.start()
-    try:
-        while True:
-            for ev in c.get_events():
-                print(ev)
-    except KeyboardInterrupt:
-        c.terminate()
+    RobotController().mainloop()
